1306ef4bd57cddf764da7d6d87be42bbfa90b481
ゴン <38377856+gon027@users.noreply.github.com>
2021-12-13 20:01:41 +0900

    Matrix4x3を使えないようにした

--- MathLib/Math/include/Math.hpp
+++ MathLib/Math/include/Math.hpp
@@ -5,8 +5,8 @@
 #include "Vector/Vector3.h"
 
 // 行列
+// #include "Matrix/Matrix4x3.h"
 #include "Matrix/Matrix4x4.h"
-#include "Matrix/Matrix4x3.h"
 
 // クォータニオン
 #include "Quaternion/Quaternion.h"
